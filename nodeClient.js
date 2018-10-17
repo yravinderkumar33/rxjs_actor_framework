92b85e1bb8351c93b4fa9d02a0508df9e978871d
yravinderkumar33 <yravinderkumar33@gmail.com>
2018-10-17 12:16:33 +0530

    final changes

--- nodeClient.js
+++ nodeClient.js
@@ -1,7 +1,7 @@
 const seneca = require('seneca');
 const nodeService = seneca().client();
 const serializeArgs = require('./serializeArgs');
-
+const uuid = require('uuid/v1');
 const data = {
     operations: [{
         operator: 'filter',
@@ -10,9 +10,11 @@ const data = {
         operator: 'map',
         args: [i => i*2]
     }],
-    values: [2, 3, 4, 5]
+    values: [2, 3, 4, 5],
+    sourceId: uuid()
 }
-
-nodeService.act('role:fw,cmd:createActor', serializeArgs(data), (err, response) => {
-    console.log('Response is:', response);
+nodeService
+.act({role:'fw',cmd:'createActor'},serializeArgs(data), (err, response) => {
+    console.log('Response is:', response.second);
 });
+
--- nodePlugin.js
+++ nodePlugin.js
@@ -1,31 +1,69 @@
 const { Subject } = require('rxjs');
-
 const deserializeArgs = require('./deserializeArgs');
+const uuidv1 = require('uuid/v1');
+
+function generateId(that){
+    return new Promise((resolve,reject)=>{ 
+    let newId;
+    that.act("role:id,cmd:generate", (err, response) => {
+        newId = response.id;
+         resolve(newId);
+    })
+    })
+}
 
 function nodePlugin() {
+
+    let nodePluginID;
+
     this.add('role:fw,cmd:createActor', (msg, response) => {
-        const m = deserializeArgs(msg);
-        const {Subject} = require('rxjs');
-        const { map, filter } = require('rxjs/operators');
+        console.log("source actor id " + nodePluginID);
 
-        const s = new Subject();
-
-        console.log('operations', m.operations[0].args[0]);
-
-        console.log('filter 3', m.operations[0].args[0](3));
-
-        const arr1 = m.operations.map(o => {
-            const op = require('rxjs/operators')[o.operator];
-            return op.apply(op, o.args); // filter(i => i%2 === 0)
+        this.act('role:fw,cmd:newActor', msg, (err, resp) => {
+            if (err) console.log(err);
+            // console.log("response from outer service", resp);
+            response({ second: resp });
         });
 
-        s.pipe.apply(s, arr1).subscribe(console.log, console.log);
+    });
 
-        m.values.forEach(i => s.next(i));
 
-        s.complete();
-                response(null, {answer: 'ok'});
+
+    this.add('role:fw,cmd:newActor', (msg, response) => {
+        let actorId;
+        generateId(this).then((newId)=>{
+            console.log("new id for new actor generated" , newId);
+            actorId = newId;
+        });
+        const m = deserializeArgs(msg);
+        const { Subject } = require('rxjs');
+        const s = new Subject();
+        const arr1 = m.operations.map(o => {
+            const op = require('rxjs/operators')[o.operator];
+            return op.apply(op, o.args);
+        });
+        s.pipe.apply(s, arr1).subscribe((data) => {
+            console.log(data);
+        }, (err) => {
+            response(err, null);
+        },
+            () => {
+                response(null, { answer: "completed" })
             });
-        }
+        m.values.forEach(i => s.next(i));
+        s.complete();
+        
+
+    });
+    
+    this.add({ init: 'nodePlugin' }, (msg, respond) => {
+        generateId(this).then((newId)=>{
+            nodePluginID = newId;
+            respond();
+        })
+        
+    })
+
+}
 
 module.exports = nodePlugin;
